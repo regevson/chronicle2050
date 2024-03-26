320dac0aa7eae1bf1a7e6ff04629fde7e200a453
regevson <juwal.regev@hotmail.com>
2024-03-26 23:03:22 +0100

    Change favicon

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,9 @@ import router from "./router";
 import VueApexCharts from "vue3-apexcharts";
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://10.0.0.242:8000';
+//axios.defaults.baseURL = 'http://10.0.0.242:8000';
 //axios.defaults.baseURL = 'http://localhost:8000';
-//axios.defaults.baseURL = 'https://chronicle2050.regevson.com';
+axios.defaults.baseURL = 'https://chronicle2050.regevson.com';
 
 
 const app = createApp(App);
